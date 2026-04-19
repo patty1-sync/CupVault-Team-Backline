2470a4233e2dd90f25b88a51783d5bbea0968738
ESTEBAN_GOMEZ <gomezperdomo.e@northeastern.edu>
2026-04-19 14:44:49 -0400

    Replaced template personas with 4 CupVault users: Jason (Fan), Maria (Analyst), Andrew (Bettor), Jake (Admin) with select widgets for role-based login

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,72 +1,64 @@
-##################################################
-# This is the main/entry-point file for the
-# sample application for your project
-##################################################
-
-# Set up basic logging infrastructure
 import logging
 logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# import the main streamlit library as well
-# as SideBarLinks function from src/modules folder
 import streamlit as st
 from modules.nav import SideBarLinks
 
-# streamlit supports regular and wide layout (how the controls
-# are organized/displayed on the screen).
 st.set_page_config(layout='wide')
-
-# If a user is at this page, we assume they are not
-# authenticated.  So we change the 'authenticated' value
-# in the streamlit session_state to false.
 st.session_state['authenticated'] = False
-
-# Use the SideBarLinks function from src/modules/nav.py to control
-# the links displayed on the left-side panel.
-# IMPORTANT: ensure src/.streamlit/config.toml sets
-# showSidebarNavigation = false in the [client] section
 SideBarLinks(show_home=True)
 
-# ***************************************************
-#    The major content of this page
-# ***************************************************
-
 logger.info("Loading the Home page of the app")
-st.title('CupVault')
-st.write('#### Hi! As which user would you like to log in?')
 
-# For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user
-# can click to MIMIC logging in as that mock user.
+st.title('⚽ CupVault')
+st.write('### Your historical World Cup data platform')
+st.write('---')
+st.write('#### Select a user and log in to get started')
 
-if st.button("Act as Jason, a Political Strategy Advisor",
-             type='primary',
-             use_container_width=True):
-    # when user clicks the button, they are now considered authenticated
-    st.session_state['authenticated'] = True
-    # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
-    # we add the first name of the user (so it can be displayed on
-    # subsequent pages).
-    st.session_state['first_name'] = 'John'
-    # finally, we ask streamlit to switch to another page, in this case, the
-    # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+fan_users     = ['Jason Rivera (Fan)']
+analyst_users = ['Maria Santos (Analyst)']
+bettor_users  = ['Andrew Chen (Bettor)']
+admin_users   = ['Jake Williams (Admin)']
 
-if st.button('Act as Mohammad, a USAID Worker',
-             type='primary',
-             use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+col1, col2 = st.columns(2)
 
-if st.button('Act as System Administrator',
-             type='primary',
-             use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'administrator'
-    st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
+with col1:
+    st.write('**🏟️ Fan**')
+    fan_selection = st.selectbox('Select Fan', fan_users, label_visibility='collapsed')
+    if st.button('Log in as Fan', type='primary', use_container_width=True):
+        st.session_state['authenticated'] = True
+        st.session_state['role'] = 'fan'
+        st.session_state['first_name'] = 'Jason'
+        st.session_state['user_id'] = 1
+        st.switch_page('pages/00_Fan_Home.py')
+
+    st.write('')
+    st.write('**📊 Sports Analyst**')
+    analyst_selection = st.selectbox('Select Analyst', analyst_users, label_visibility='collapsed')
+    if st.button('Log in as Analyst', type='primary', use_container_width=True):
+        st.session_state['authenticated'] = True
+        st.session_state['role'] = 'analyst'
+        st.session_state['first_name'] = 'Maria'
+        st.session_state['user_id'] = 2
+        st.switch_page('pages/10_Analyst_Home.py')
+
+with col2:
+    st.write('**💰 Sports Bettor**')
+    bettor_selection = st.selectbox('Select Bettor', bettor_users, label_visibility='collapsed')
+    if st.button('Log in as Bettor', type='primary', use_container_width=True):
+        st.session_state['authenticated'] = True
+        st.session_state['role'] = 'bettor'
+        st.session_state['first_name'] = 'Andrew'
+        st.session_state['user_id'] = 3
+        st.switch_page('pages/20_Bettor_Home.py')
+
+    st.write('')
+    st.write('**🛠️ Database Admin**')
+    admin_selection = st.selectbox('Select Admin', admin_users, label_visibility='collapsed')
+    if st.button('Log in as Admin', type='primary', use_container_width=True):
+        st.session_state['authenticated'] = True
+        st.session_state['role'] = 'admin'
+        st.session_state['first_name'] = 'Jake'
+        st.session_state['user_id'] = 4
+        st.switch_page('pages/30_Admin_Home.py')
